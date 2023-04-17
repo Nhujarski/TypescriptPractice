cf039a9fa23ecbcbe6ff08e19aa5929efb770178
Nick Hujarski <nickhujarski@Roberts-13-inch-MacBook-Pro.local>
2023-04-16 19:15:06 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {User} from './User'
+import {Company} from './Company'
+/// <reference types="@types/google.maps" />
+
+let user = new User()
+let company = new Company()
+console.log({user})
+console.log({company})
+
